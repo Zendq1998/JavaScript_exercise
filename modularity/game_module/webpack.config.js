f265c2c2ded53dafa33ef9e20f6f2f5ea5564715
KuTuGu <256317135@qq.com>
2018-03-30 15:46:00 +0800

    3.30

---
+++ modularity/game_module/webpack.config.js
@@ -0,0 +1,28 @@
+//引入html插件
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+    entry: "./src/game.js",
+    output: {
+        path: __dirname + '/dist',
+        filename: "game.js"
+    },
+    plugins: [new HtmlWebpackPlugin({//html插件配置
+        filename: 'game.html',
+        template: './src/game.html',
+        minify: {
+            //压缩
+            collapseWhitespace: true
+        }
+    }
+    )],
+    module: {
+        rules: [
+          {//css插件配置
+            test: /\.css$/,
+            use: [ 'style-loader', 'css-loader' ]
+          }
+        ]
+    },
+    //配置json中的"scripts"命令(重命名)
+    mode: 'development'
+};
